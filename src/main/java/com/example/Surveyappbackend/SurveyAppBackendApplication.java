62de7d7ab6e2f4a5d3a3e636301035dc08d1584e
iliass <iliasses-saoury@protonmail.com>
2023-04-27 16:23:01 +0000

    initial commit

---
+++ src/main/java/com/example/Surveyappbackend/SurveyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Surveyappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Surveyappbackend/SurveyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Surveyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
