9067376aef4891b8f7f9276d896a7beb41147255
Laaqira <laaqiraamine@gmail.com>
2023-05-04 18:51:49 +0100

    place repositories in their folder

--- src/main/java/com/example/Surveyappbackend/repository/OrganisationRepository.java
+++ src/main/java/com/example/Surveyappbackend/repository/OrganisationRepository.java
@@ -1,5 +1,6 @@
-package com.example.Surveyappbackend.model;
+package com.example.Surveyappbackend.repository;
 
+import com.example.Surveyappbackend.model.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
